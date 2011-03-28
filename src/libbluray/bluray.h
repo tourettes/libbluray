f2ffbb7cac6a671b822df8dd8716bbd54ff956e5
Hendrik Leppkes <h.leppkes@gmail.com>
2011-03-28 22:39:52 +0200

    Added bd_get_clip_infos
    This function allows for querying information directly related to the
    clips inside a title.

--- src/libbluray/bluray.h
+++ src/libbluray/bluray.h
@@ -943,6 +943,18 @@ void bd_free_bdjo(struct bdjo_data *);
 int  bd_start_bdj(BLURAY *bd, const char* start_object); // start BD-J from the specified BD-J object (should be a 5 character string)
 void bd_stop_bdj(BLURAY *bd); // shutdown BD-J and clean up resources
 
+/**
+ *
+ * Get information about the clip
+ *
+ * @param bd  BLURAY object
+ * @param clip clip index
+ * @param clip_start_time start of the clip (in the total title) (in 90khz)
+ * @param stream_start_time first pts in the clip (in 90khz)
+ * @param byte position of the clip (absolute)
+ * @param duration duration of the clip (in 90khz)
+ */
+int bd_get_clip_infos(BLURAY *bd, int clip, uint64_t *clip_start_time, uint64_t *stream_start_time, uint64_t *pos, uint64_t *duration);
 
 #ifdef __cplusplus
 };
