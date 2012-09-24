f83e051511d95336a45079307e1142b350b1271d
Hendrik Leppkes <h.leppkes@gmail.com>
2012-09-24 02:01:25 +0200

    Include clpi_data.h to make debugging easier.

--- src/libbluray/bluray.h
+++ src/libbluray/bluray.h
@@ -32,6 +32,7 @@ extern "C" {
  */
 
 #include <stdint.h>
+#include "bdnav/clpi_data.h"
 
 #define TITLES_ALL              0    /**< all titles. */
 #define TITLES_FILTER_DUP_TITLE 0x01 /**< remove duplicate titles. */
@@ -1026,7 +1027,6 @@ int bd_mouse_select(BLURAY *bd, int64_t pts, uint16_t x, uint16_t y);
 
 /* access to internal information */
 
-struct clpi_cl;
 /**
  *
  *  Get copy of clip information for requested playitem.
